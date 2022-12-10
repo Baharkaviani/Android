8a4a893cc72f3943865c4440355722ed405016c1
baharKaviani <bhr.kaviani@gmail.com>
2022-12-10 07:35:10 +0330

    add: import androidx.navigation

--- HafezDivination/app/src/main/java/com/example/hafezdivination/MainActivity.kt
+++ HafezDivination/app/src/main/java/com/example/hafezdivination/MainActivity.kt
@@ -2,8 +2,18 @@ package com.example.hafezdivination
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+//import com.example.hafezdivination.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+//    private lateinit var appBarConfiguration: AppBarConfiguration
+//    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
