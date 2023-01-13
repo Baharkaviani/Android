01c5c8d237065861a84db9c65103e1822e757f33
baharKaviani <bhr.kaviani@gmail.com>
2023-01-13 22:06:11 +0330

    edit: change the view of result section

--- WeatherForecast/app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ WeatherForecast/app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -204,39 +204,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadImage(icon: String) {
-        val imageView = binding.todayIcon
-
-        // Declaring executor to parse the URL
         val executor = Executors.newSingleThreadExecutor()
-
-        // Once the executor parses the URL
-        // and receives the image, handler will load it
-        // in the ImageView
         val handler = Handler(Looper.getMainLooper())
-
-        // Initializing the image
         var image: Bitmap? = null
+        print("icon printed")
+        print(icon)
 
-        // Only for Background process (can take time depending on the Internet speed)
         executor.execute {
-
-            // Image URL
             val imageURL = String.format(iconUrl, icon)
 
-            // Tries to get the image and post it in the ImageView
-            // with the help of Handler
+            // Tries to get the image and post it in the ImageView with the help of Handler
             try {
                 val `in` = java.net.URL(imageURL).openStream()
                 image = BitmapFactory.decodeStream(`in`)
 
-                // Only for making changes in UI
                 handler.post {
-                    imageView.setImageBitmap(image)
+                    binding.todayIcon.setImageBitmap(image)
                 }
             }
-
-            // If the URL does not point to
-            // image or any other kind of failure
             catch (e: Exception) {
                 e.printStackTrace()
             }
